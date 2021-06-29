28f4d58f40ee3b52019c1ae62192744229006ad7
HOGAKE-ke-7 <adiisrahadi@gmail.com>
2021-06-29 14:21:11 +0700

    firebase navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div id="app">
     <div id="nav">
+      <Navbar/>
       <router-link to="/">Home</router-link> |
     </div>
     <router-view/>
   </div>
 </template>
 
+<script>
+import Navbar from '@/components/Navbar.vue'
+export default {
+  components: { Navbar }
+
+}
+</script>
+
 <style>
-</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <nav class="navbar fixed-top navbar-light bg-light">
+  <div class="container-fluid">
+    <h1>halo masuk navbar</h1>
+    <a class="navbar-brand" href="#">Fixed top</a>
+  </div>
+</nav>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
