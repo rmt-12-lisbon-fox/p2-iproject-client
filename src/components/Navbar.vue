17b2e5f4eb61733b905e0e277a5511e4a64c57b9
HOGAKE-ke-7 <adiisrahadi@gmail.com>
2021-06-29 14:44:39 +0700

    FIREBASE DEPLOY

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,26 @@
 <template>
   <section>
-    <nav class="navbar fixed-top navbar-light bg-light">
+    <nav class="navbar fixed-top navbar-dark bg-dark pt-3 pb-3">
   <div class="container-fluid">
-    <h1>halo masuk navbar</h1>
-    <a class="navbar-brand" href="#">Fixed top</a>
+    <h2 class="text-white h2">Smart Investment</h2>
+    <div class="d-flex justify-content-around col-sm-4">
+    <a class="navbar-brand h2" >Market</a>
+    <a class="navbar-brand h2" >Signal</a>
+    <a class="navbar-brand h2" >Sign In</a>
+    </div>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <div class="d-flex justify-content-end">
+        <a class="nav-link active pointer" >Portofolio</a>
+        </div>
+        <div class="d-flex justify-content-end">
+        <a class="nav-link active pointer" >Sign Out</a>
+        </div>
+      </div>
+    </div>
   </div>
 </nav>
   </section>
@@ -16,5 +33,17 @@ export default {
 </script>
 
 <style>
-
+.h2{
+  backgorund-color : black;
+  text-color: white;
+cursor: pointer
+}
+.h2:hover{
+  backgorund-color : blue;
+  text-color: blue;
+cursor: pointer
+}
+.pointer{
+  cursor: pointer;
+}
 </style>
