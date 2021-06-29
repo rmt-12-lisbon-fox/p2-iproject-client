db3f6f213b57691062bbb9154d95d4a121a6c5f6
amrrobb <amr.robb@gmail.com>
2021-06-29 19:17:51 +0700

    add navbar style

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,82 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <nav-bar/>
     <router-view/>
 
-    <HFooter class="fixed-bottom"/>
+    <!-- <ul>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+    </ul>
+    <h3>Essential Links</h3>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+    </ul> -->
+
+    <HFooter id="footer"/>
   </div>
 </template>
 
 <script>
 import HFooter from 'vue-hacktiv8-footer'
+import NavBar from './components/NavBar.vue'
 
 export default {
   components: {
-    HFooter
+    HFooter, NavBar
   }
 }
 </script>
 
 <style>
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+@import url('https://fonts.googleapis.com/css2?family=Baloo+2:wght@400;500;600&display=swap');
+
+#app {
+  font-family: 'Baloo 2', cursive, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #f5f5f5 !important;
 }
 
-#nav {
+body {
+  background-image: linear-gradient(#09244a, #124791);
+  min-height: 1080px;
+  /* height: 100%; */
+}
+
+.nav {
+  font-size: 1.4em;
   padding: 30px;
+  font-weight: 400;
+  color: #ffffff;
+
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.nav a {
+  font-weight: 400;
+  color: #ffffff;
+  opacity: 0.75;
+
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-} */
+.nav a.router-link-exact-active {
+  font-weight: 600;
+  color: #ffffff;
+  opacity: 1;
+}
+
+#footer {
+  position: absolute;
+  display: flex;
+  bottom: 0;
+  width: 100%;
+  /* height: 2.5rem;   */
+  /* margin-top: 5%; */
+}
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-transparent p-4 nav">
+  <div class="container-fluid d-flex justify-conternt-evenly">
+    <div class="d-flex justify-content-between align-items-center">
+        <div class="d-flex align-items-center mx-3">
+            <img src="../assets/logo.png" alt="" width="40px" height="40px" class="me-3"> 
+            <p class="h3 m-0"><b>Aniplay</b></p>
+        </div>
+        
+
+        <form class="d-flex mx-5">
+            <input class="form-control form-control-sm mx-2" type="search" placeholder="Search anime title.." aria-label="Search">
+            <button class="btn btn-light" type="submit">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                </svg>
+            </button>
+        </form>
+        
+    </div>  
+
+    <div class="me-3" >
+      <ul class="navbar-nav mx-auto">
+        <li class="nav-item mx-2">
+          <router-link class="nav-link" to='/'>Home</router-link>
+        </li>
+        <li class="nav-item mx-2">
+          <router-link class="nav-link" to='/about'>Link</router-link>
+        </li>
+        <li class="nav-item mx-2">
+          <router-link class="nav-link" to='/detail'> Dropdown </router-link>
+        </li>
+        <!-- <li class="nav-item mx-2">
+          <router-link class="nav-link" to='/'>Disabled</router-link>
+        </li> -->
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+    name: 'NavBar'
+}
+</script>
+
+<style>
+
+</style>
