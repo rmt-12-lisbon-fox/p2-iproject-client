04148b7dae16a893a8a4c7757e1ea710a2409a8a
HOGAKE-ke-7 <adiisrahadi@gmail.com>
2021-06-29 14:59:39 +0700

    firebase deploy completed

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <div id="app">
     <div id="nav">
       <Navbar/>
-      <router-link to="/">Home</router-link> |
     </div>
     <router-view/>
   </div>
@@ -11,6 +10,7 @@
 <script>
 import Navbar from '@/components/Navbar.vue'
 export default {
+  name: 'App',
   components: { Navbar }
 
 }
