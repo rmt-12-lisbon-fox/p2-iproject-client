32b1553dffab7627c42f5db00d160b1cf95a6d4d
Neovastor <neovastr1997@gmail.com>
2021-06-30 00:42:18 +0700

    first

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-info p-3">
+    <a class="navbar-brand text-white" href="#">Safe Travel</a>
+
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a
+            class="nav-link text-white"
+            href="#"
+            aria-disabled="true"
+            @click="toLoginPage"
+            >Login</a
+          >
+        </li>
+        <li class="nav-item">
+          <a
+            @click="logout"
+            class="nav-link text-white"
+            href="#"
+            aria-disabled="true"
+            >Logout</a
+          >
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  methods: {
+    toLoginPage() {
+      this.$router.push("/login").catch(() => {});
+    },
+    logout() {
+      localStorage.clear();
+      this.$store.commit("LOGOUT");
+      this.$router.push("/login");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
