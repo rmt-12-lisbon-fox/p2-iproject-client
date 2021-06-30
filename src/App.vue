61cea85fe725ebfc8ca407efb79de52b6ef47a8f
HOGAKE-ke-7 <adiisrahadi@gmail.com>
2021-07-01 03:49:02 +0700

    Hfooter

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,18 @@
       <Navbar/>
     </div>
     <router-view/>
+    <div id="foot">
+      <HFooter/>
+    </div>
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar.vue'
+import HFooter from 'vue-hacktiv8-footer'
 export default {
   name: 'App',
-  components: { Navbar },
+  components: { Navbar, HFooter },
   created () {
     if (localStorage.access_token) {
       this.$store.commit('ISLOGIN')
