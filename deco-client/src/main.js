cce17aac83ead6971a80345b1b4b0586175d1f23
jeffsabarman <jeffsabarman@gmail.com>
2021-06-30 14:45:09 +0700

    add webrtc

--- deco-client/src/main.js
+++ deco-client/src/main.js
@@ -4,8 +4,14 @@ import router from './router'
 import store from './store'
 import GoogleSignInButton from 'vue-google-signin-button-directive'
 import VueChatScroll from 'vue-chat-scroll'
+import WebRTC from 'vue-webrtc'
+import * as io from 'socket.io-client'
+
+window.io = io
+
 
 Vue.use(VueChatScroll)
+Vue.use(WebRTC)
 
 Vue.config.productionTip = false
 
