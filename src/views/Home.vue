4db4fdda2255d7c51d106a2052d503e190c4ee2e
amrrobb <amr.robb@gmail.com>
2021-06-30 19:45:20 +0700

    add episode list for stream & download

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,40 @@
 <template>
-  <div class="home">
+  <div id="home-page">
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="mb-5">
+      <h2 class="display-6">Upcoming Anime</h2>
+    </div>
+    <div class="container">
+      <div class="row row-cols-5 gy-4">
+          <AnimeCard v-for="el in upcoming" 
+            :key="el.mal_id" :el="el"
+          />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import {mapState} from 'vuex'
+import AnimeCard from '../components/AnimeCard.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    AnimeCard
+  },
+  computed: {
+    ...mapState(['upcoming'])
   },
   created() {
     this.$store.dispatch('upcomingAnime')
   }
 }
 </script>
+
+<style>
+#home-page{
+  padding-bottom: 8%;
+}
+
+</style>
