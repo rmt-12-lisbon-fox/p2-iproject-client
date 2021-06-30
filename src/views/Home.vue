7481d816fd317fe3ca2326dd8d99620aefa5aa04
HOGAKE-ke-7 <adiisrahadi@gmail.com>
2021-06-30 10:17:31 +0700

    market OK

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,10 +8,11 @@
     </ol>
     <div class="carousel-inner" role="listbox">
       <!-- Slide One - Set the background image for this slide in the line below -->
-      <div class="carousel-item active" style="background-image: url('https://source.unsplash.com/RCAhiGJsUUE/1920x1080')">
+      <div class="carousel-item active" style="background-image: url('https://images.unsplash.com/photo-1617854611335-e54f6f1b1f42?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80')">
+      <!-- <div class="carousel-item active" style="background-image: url('https://source.unsplash.com/RCAhiGJsUUE/1920x1080')"> -->
         <div class="carousel-caption d-none d-md-block">
-          <h3 class="display-4">Change strategy and customizable</h3>
-          <p class="lead">Accurate signal never seen before</p>
+          <!-- <h3 class="display-4 text-dark fw-bold">Change strategy and customizable</h3>
+          <p class="lead">Accurate signal never seen before</p> -->
         </div>
       </div>
       <!-- Slide Two - Set the background image for this slide in the line below -->
@@ -42,8 +43,9 @@
 <!-- Page Content -->
 <section class="py-5">
   <div class="container">
-    <h1 class="font-weight-light">Accurate signal never seen before</h1>
-    <p class="lead">Unlike all others website signal, here, provide more active orders</p>
+    <h1 class="font-weight-light text-dark">Accurate signal never seen before</h1>
+    <p class="lead text-dark">Unlike all others website signal, here, provide more active orders</p>
+    <p class="lead text-dark">Change strategy and customizable</p>
     <!-- <p class="lead">lorem ipsum dumet color <a href="https://unsplash.com">Unsplash</a>!</p> -->
   </div>
 </section>
