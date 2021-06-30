95b553f3f9e20f0b5fce819f43e1f1d254353708
tiansyahmsalman <tiansyahisal@gmail.com>
2021-07-01 04:12:26 +0700

    ready deploy

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,7 +2,7 @@
         <div class="col-4 d-flex flex-column justify-content-center align-items-center my-4">
           <img :src="community.imageUrl" class="img-thumbnail rounded-circle mb-3" width="300">
           <h3>{{community.name}}</h3> <p class="text-center mt-2">{{community.description}}</p>
-          <button class="btn btn-warning" @click.prevent="addMyCommunity(community.id)">Join</button>
+          <button class="btn btn-warning" @click.prevent="addMyCommunity(community.id)">Join Community</button>
         </div>
 </template>
 
