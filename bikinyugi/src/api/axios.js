84107aaba65bdf194b53be756d0d47049e37a92e
farel14 <72905415+farel14@users.noreply.github.com>
2021-07-02 10:27:30 +0700

    update wiki & builder

--- bikinyugi/src/api/axios.js
+++ bikinyugi/src/api/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 const axi = axios.create({
-  // baseURL: 'https://kodok-sarjana.herokuapp.com/',
-  baseURL: 'http://localhost:3000/',
-  headers: {access_token: localStorage.access_token}
+  baseURL: 'https://kodok-sarjana.herokuapp.com/',
+  // baseURL: 'http://localhost:3000/',
+  headers: { access_token: localStorage.access_token }
 })
 
 export default axi
